72525a7b185716afe5f862aff4789f2f8f2c33f7
Abhishekraval3 <83743390+Abhishekraval3@users.noreply.github.com>
2022-07-20 12:35:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,31 +9,26 @@ model_dt = pickle.load(open('model_dt.pkl', 'rb'))
 model_knn = pickle.load(open('model_knn.pkl', 'rb'))
 model_svm = pickle.load(open('model_svm.pkl', 'rb'))
 model_lr = pickle.load(open('model_lr.pkl', 'rb'))
+model_stack = pickle.load(open('model_stack.pkl','rb'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/predict',methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    pr_lr = model_lr.predict(final_features)
-    pr_dt = model_dt.predict(final_features)
-    pr_knn = model_knn.predict(final_features)
-    pr_svm = model_svm.predict(final_features)
-    pr_rf = model_rf.predict(final_features)
+    prediction = model_stack.predict(final_features)
 
-    prediction = (pr_lr + pr_dt + pr_knn + pr_svm + pr_rf)
-    output = prediction/5 
-    
-    if output>0.5:
-        res = "Likely to have a Heart Disease"
+    if prediction == 1:
+        res = "Likely to have a cardiovascular Disease"
     else:
         res = "You are Healthy"
-    return render_template('index.html', prediction_text = res)
 
+   
+    return render_template('index.html', prediction_text = res)
 
 if __name__ == "__main__":
     app.run(debug=True,port=8088)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.metrics import accuracy_score
 
 # imporing the dataset
 df = pd.read_csv('framingham.csv')
@@ -11,7 +12,7 @@ df.corr()
 
 #feature selection by droping unwanted features
 df.drop(columns=['education'],inplace=True)
-df.drop(columns=['cigsPerDay','diaBP'],inplace=True)
+df.drop(columns=['cigsPerDay'],inplace=True)
 
 # Replaceing all null values with mean of the features
 df.replace(np.nan,df.mean(),inplace=True)
@@ -38,42 +39,95 @@ lim = 3041
 x_train = x[:lim]
 y_train = y[:lim]
 
-#testing data
+# Testing data
 x_test = x[lim:]
 y_test = y[lim:]
 
-
-##Pre-processing
-from sklearn.preprocessing import StandardScaler
-s=StandardScaler()
-x_train=s.fit_transform(x_train)
-x_test=s.fit_transform(x_test)
 
 ## Logistic Regression Algorithm
 from sklearn.linear_model import LogisticRegression
 l = LogisticRegression(solver='liblinear',max_iter = 1000, random_state = 31)
 l.fit(x_train,y_train)
+lr_train_pred = l.predict(x_train)
+lr_test_pred = l.predict(x_test)
+lr_train_acc = round(accuracy_score(lr_train_pred,y_train)*100,2)
+lr_test_acc = round(accuracy_score(lr_test_pred,y_test)*100,2)
+
 
 # KNN Algorithm
 from sklearn.neighbors import KNeighborsClassifier
-k=KNeighborsClassifier(n_neighbors=12)
+k=KNeighborsClassifier(n_neighbors=14)
 k.fit(x_train,y_train)
+knn_train_pred = k.predict(x_train)
+knn_test_pred = k.predict(x_test)
+knn_train_acc = round(accuracy_score(knn_train_pred,y_train)*100,2)
+knn_test_acc = round(accuracy_score(knn_test_pred,y_test)*100,2)
+
 
 # Decision Tree ALgorithm
 from sklearn.tree import DecisionTreeClassifier
 d=DecisionTreeClassifier()
 d.fit(x_train,y_train)
+dt_train_pred = d.predict(x_train)
+dt_test_pred = d.predict(x_test)
+dt_train_acc = round(accuracy_score(dt_train_pred,y_train)*100,2)
+dt_test_acc = round(accuracy_score(dt_test_pred,y_test)*100,2)
+
 
 # Support Vector Machine Algorithm
 from sklearn.svm import SVC
 s=SVC(C=10,kernel="linear",gamma="auto")
 s.fit(x_train,y_train)
+svm_train_pred = s.predict(x_train)
+svm_test_pred = s.predict(x_test)
+svm_train_acc = round(accuracy_score(svm_train_pred,y_train)*100,2)
+svm_test_acc = round(accuracy_score(svm_test_pred,y_test)*100,2)
+
 
 # Random Forest Algorithm
 from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier(random_state=168)
+rf = RandomForestClassifier(random_state=160)
+rfr = RandomForestClassifier(random_state=168)
 rf.fit(x_train,y_train)
+rf_train_pred = rf.predict(x_train)
+rf_test_pred = rf.predict(x_test)
+rf_train_acc = round(accuracy_score(rf_train_pred,y_train)*100,2)
+rf_test_acc = round(accuracy_score(rf_test_pred,y_test)*100,2)
 
+#Ensamble Model using Stacking
+from sklearn.ensemble import StackingClassifier
+estimators = {('knn',k),
+             ('svm',s),
+              ('dt',d),
+              ('lr',l),
+              ('rf',rfr)}
+
+stack_model = StackingClassifier(
+estimators = estimators, final_estimator = LogisticRegression(random_state=22))
+stack_model.fit(x_train,y_train)
+stack_train_pred = stack_model.predict(x_train)
+stack_test_pred = stack_model.predict(x_test)
+stack_train_acc = round(accuracy_score(stack_train_pred,y_train)*100,2)
+stack_test_acc = round(accuracy_score(stack_test_pred,y_test)*100,2)
+
+#printing Accuracy
+print('LR_training_acc',lr_train_acc)
+print('LR_test_acc',lr_test_acc)
+
+print('KNN_training_acc',knn_train_acc)
+print('KNN_test_acc',knn_test_acc)
+
+print('DT_training_acc',dt_train_acc)
+print('DT_test_acc',dt_test_acc)
+
+print('SVM_training_acc',svm_train_acc)
+print('SVM_test_acc',svm_test_acc)
+
+print('RF_training_acc',rf_train_acc)
+print('RF_test_acc',rf_test_acc)
+
+print('STACK_training_acc',stack_train_acc)
+print('STACK_test_acc',stack_test_acc)
 
 ## dumpling the model in pickle file
 pickle.dump(l, open('model_lr.pkl','wb'))
@@ -81,4 +135,4 @@ pickle.dump(k, open('model_knn.pkl','wb'))
 pickle.dump(d, open('model_dt.pkl','wb'))
 pickle.dump(s, open('model_svm.pkl','wb'))
 pickle.dump(rf, open('model_rf.pkl','wb'))
-
+pickle.dump(stack_model,open('model_stack.pkl','wb'))
